0753cd2e0b65babecef084f86b921959319553ad
Ashlynn Anderson <pea@pea.sh>
2022-08-21 17:23:40 -0400

    Simplify loadup.el
    I didn't fully understand how guile loads modules when initially
    writing it

--- scheme/loadup.scm
+++ scheme/loadup.scm
@@ -1,4 +1,1 @@
-(let ([scheme-dir (dirname (current-filename))])
-  (add-to-load-path scheme-dir)
-  (add-to-load-path (string-append scheme-dir "wl/"))
-  (add-to-load-path (string-append scheme-dir "winter/")))
+(add-to-load-path (dirname (current-filename)))
