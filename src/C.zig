c3128b58a0dbe3e5afb63c9dfebe8ae04ac595eb
Ashlynn Anderson <pea@pea.sh>
2022-08-21 19:01:31 -0400

    undef SCM_C_INLINE in cImport
    Should improve zig's ability to translate the headers

--- src/C.zig
+++ src/C.zig
@@ -1,3 +1,6 @@
+// Disabling inlining makes zig translate-c's job much easier
 pub usingnamespace @cImport({
+    @cInclude("libguile/scmconfig.h");
+    @cUndef("SCM_C_INLINE");
     @cInclude("libguile.h");
 });
